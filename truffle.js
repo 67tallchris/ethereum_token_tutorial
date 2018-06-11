faed7578bdde21f900f1eecc30cb4bfea292dbf5
Christopher Porter <chris@Christophers-MacBook-Pro-2.local>
2018-06-10 18:55:01 -0700

    added boilerplate code to truffle.js

--- truffle.js
+++ truffle.js
@@ -1,4 +1,18 @@
+// truffle.js
+require('dotenv').config();
+const HDWalletProvider = require("truffle-hdwallet-provider");
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: "localhost",
+      port: 7545,
+      gas: 6500000,
+      network_id: "5777"
+    },
+    ropsten: {
+        provider: new HDWalletProvider(process.env.MNENOMIC, "https://ropsten.infura.io/" + process.env.INFURA_API_KEY),
+        network_id: 3,
+        gas: 4500000
+    },
+  }
 };
